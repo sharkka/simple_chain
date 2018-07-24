f24be585b068c44683c9e88ed799cdb9f60a0121
anyongzhi <yongzhi.an@hobot.cc>
2018-07-24 11:47:00 +0800

    response pack

--- src/chaintest.cpp
+++ src/chaintest.cpp
@@ -7,6 +7,17 @@
  */
 #include "simple_chain_service.h"
 #include "response_services.h"
+
+
+struct resp_pack_t {
+    const char*      url;
+    ResponseCallback resp_cb;
+
+    resp_pack_t(const char* url, ResponseCallback cb) 
+    : url(url), resp_cb(cb) {
+    }
+};
+
 /**
  * @Method   http_server_test
  * @Brief    
@@ -16,21 +27,20 @@
  * @Keyword
  */
 static void http_server_test() {
-    const char* url_01 = "/version";
-    const char* url_02 = "/init";
-    const char* url_03 = "/index";
-    const char* url_04 = "/help";
-    const char* url_05 = "/haveatry";
-    const char* url_06 = "/chaining";
-    //const char* url_077 = "/error";
+    resp_pack_t resp[] = {
+        {"/version", response_version},
+        {"/init", response_init},
+        {"/index", response_index},
+        {"/help", response_help},
+        {"/haveatry", response_haveatry},
+        {"/chaining", response_chaining}
+    };
 
     simple_chain_service scs(9090);
-    scs.addService(url_01, response_version);
-    scs.addService(url_02, response_init);
-    scs.addService(url_03, response_index);
-    scs.addService(url_04, response_help);
-    scs.addService(url_05, response_haveatry);
-    scs.addService(url_06, response_chaining);
+    for (unsigned int i = 0; i < sizeof(resp) / sizeof(resp_pack_t); ++i) {
+        scs.addService(resp[i].url, resp[i].resp_cb);
+    }
+
     scs.start();
 }
 
